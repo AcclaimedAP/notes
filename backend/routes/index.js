e040ccca7c6a812d77737beb2f4744a947e85b42
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-04-03 20:32:18 +0200

    added mysql connection

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const mysql = require('mysql');
 
 const DATABASE_NAME = 'notesdb'
-
+const USERNAME_TABLE = 'users'
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -17,12 +17,20 @@ con.connect(function(err) {
   console.log("Connected!");
   con.query(`CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME}`, function (err, result) {
     if (err) throw err;
+      
     if(result.warningCount = 0) {
       console.log("Database created");
     } else {
       console.log("Database found");
     }
-    
+    con.changeUser({database : DATABASE_NAME}, function(err) {
+      if (err) throw err;
+      var sql = `CREATE TABLE IF NOT EXISTS ${USERNAME_TABLE} (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(32), password VARCHAR(32))`;
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+      })
+    });
   });
 });
 /* GET home page. */
