34c1143cff2c228fee1c9de49786996432ff90aa
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-04-04 05:15:53 +0200

    added login

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -25,10 +25,11 @@ router.post('/new', async function (req, res) {
   }
   var sql = `SELECT * FROM users WHERE username = ${mysql.escape(user.username)}`;
   con.query(sql, function (err, result) {
+    if (err) throw err;
     if (result.length > 0) {
       res.send({success: false, user: user.username})
     } else {
-    sql = `INSERT INTO users (username, password) VALUES ('${mysql.escape(user.username)}', '${mysql.escape(user.password)}')`;
+    sql = `INSERT INTO users (username, password) VALUES (${mysql.escape(user.username)}, ${mysql.escape(user.password)})`;
   
     con.query(sql, function (err) {
       if (err) throw err;
@@ -40,13 +41,31 @@ router.post('/new', async function (req, res) {
   
 });
 
+router.post('/login', async function (req, res) {
+  const user = {
+    username: req.body.username,
+    password: req.body.password
+  }
+  sql = `SELECT * FROM users WHERE username = ${mysql.escape(req.body.username)}`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+
+    if (result[0].password == user.password) {
+      res.send({ loginSuccessful: true, id: result[0].userID });
+    } else {
+      res.send({ loginSuccessful: false, id: null });
+    }
+  })
+
+});
+
 router.get('/all', async function (req, res) {
-    sql = `SELECT * FROM users`;
-  con.query(sql, function (err,result) {
+  sql = `SELECT * FROM users`;
+  con.query(sql, function (err, result) {
     if (err) throw err;
     res.send(result);
   });
       
-})
+});
 
 module.exports = router;
