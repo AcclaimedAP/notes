640f500516f6067e21e7868bc63b3cd854bf167f
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-04-05 19:21:40 +0200

    added function to add new documents

--- frontend/script/pages/editDocuments.js
+++ frontend/script/pages/editDocuments.js
@@ -1,3 +1,5 @@
+import { readFromLocalstorage } from "../functions";
+import { postNewDocument } from "../services/documentsServices";
 import { navbar } from "./universal";
 
 function editDocument(dom) {
@@ -6,14 +8,33 @@ function editDocument(dom) {
     const titleInput = document.createElement('input');
     const descInput = document.createElement('textarea');
     const textarea = document.createElement('textarea');
+    const submit = document.createElement('button');
     textarea.setAttribute('id', 'textContent');
     form.appendChild(titleInput);
     form.appendChild(descInput);
     form.appendChild(textarea);
+    form.appendChild(submit);
     dom.appendChild(form);
+    submit.innerHTML = 'Save';
     tinymce.init({
-        selector: '#textContent'
+        selector: '#textContent',
+
+        setp: function (editor) {
+            editor.on("change", function () {
+                editor.save();
+            })
+        }
     })
+
+    submit.addEventListener('click', async function (e) {
+        e.preventDefault();
+        var content = tinymce.get('textContent').getContent();
+        const response = await postNewDocument(readFromLocalstorage("id"), titleInput.value, descInput.value, content);
+        console.log(response);
+        if (response.success) {
+            form.remove();
+        }
+    });
 }
 
 
--- frontend/script/services/documentsServices.js
+++ frontend/script/services/documentsServices.js
@@ -13,4 +13,24 @@ function getDocumentsByAuthor(id) {
     })
 }
 
-export { getDocumentsByAuthor }
+function postNewDocument(id, docTitle = "New document", docDesc = "", docContent = "") {
+    const doc = {
+        authorId: id,
+        title: docTitle,
+        description: docDesc,
+        content: docContent
+    };
+    return fetch('http://localhost:3000/documents/new', {
+        
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(doc)
+    }).then (res => res.json())
+        .then(res => {
+            return res;
+    })
+}
+
+export { getDocumentsByAuthor, postNewDocument }
